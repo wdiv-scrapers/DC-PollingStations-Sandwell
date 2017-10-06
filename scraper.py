e3704b8a841a75fe5d2b97d678e97147aeeeb13c
chris48s <chris.shaw480@gmail.com>
2017-10-06 15:43:31 +0100

    correct local auth code

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ from dc_base_scrapers.geojson_scraper import RandomIdGeoJSONScraper
 
 
 districts_url = "http://inspire.misoportal.com/geoserver/sandwell_metropolitan_borough_council_polling_districts_polygon/ows?service=WFS&version=1.1.1&request=GetFeature&outputFormat=json&srsName=EPSG%3A4326&typeNames=sandwell_metropolitan_borough_council_polling_districts_polygon"
-council_id = 'E06000044'
+council_id = 'E08000028'
 
 
 districts_scraper = RandomIdGeoJSONScraper(districts_url, council_id, 'utf-8', 'districts')
